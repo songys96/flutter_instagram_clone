975680082cfde2994288213fd3d109127d9ae3db
yunsang <songys96@naver.com>
2021-01-12 02:08:04 +0900

    12.app bar with cupertino

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(
+            CupertinoIcons.camera,
+            color: Colors.black87,
+          ),
+        ),
+
+        middle: Text(
             "instagram",
           style: TextStyle(fontFamily: 'InstaStyle'),
+        ),
+
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/add.png'),
+                color:Colors.black87,
+              ),
+            )
+          ],
         ),
       ),
       body: ListView.builder(
