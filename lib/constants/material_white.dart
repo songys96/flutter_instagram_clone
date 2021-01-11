065df8b4ca4781b81d93680c86b08ad3dab05400
yunsang <songys96@naver.com>
2021-01-11 20:19:04 +0900

    10. custom material color

---
+++ lib/constants/material_white.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor white = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0x0FFFFFFF),
+    100: Color(0x2FFFFFFF),
+    200: Color(0x4FFFFFFF),
+    300: Color(0x6FFFFFFF),
+    400: Color(0x8FFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xAFFFFFFF),
+    700: Color(0xCFFFFFFF),
+    800: Color(0xDFFFFFFF),
+    900: Color(0xEFFFFFFF),
+  },
+);
