70395adb327fa2469488e8b21d5ad92a629ed0dd
yunsang <songys96@naver.com>
2021-01-12 03:07:09 +0900

    15. loading widget

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -3,16 +3,30 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class Post extends StatelessWidget {
   final int index;
+  Size size;
 
-  const Post(this.index,
+  Post(this.index,
       {
         Key key,
       }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (size==null) size = MediaQuery.of(context).size;
+
     return CachedNetworkImage(
-      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      imageUrl: 'https://picsum.photos/id/$index/600/600',
+      placeholder: (BuildContext context, String url){
+        return Container(
+            width: size.width,
+            height: size.width,
+            child: Center(
+                child: SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(
+                        backgroundColor: Colors.black87))));
+      },
       imageBuilder: ( BuildContext context, ImageProvider imageProvider,){
         return AspectRatio(
           aspectRatio:1,
