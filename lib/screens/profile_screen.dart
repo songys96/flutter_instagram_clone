5bb0adfb330ca99dedb43fe1a1a77e867ea3515d
yunsang <songys96@naver.com>
2021-01-24 10:11:44 +0900

    23. username in profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/constants/common_size.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -7,11 +8,20 @@ class ProfileScreen extends StatelessWidget {
       backgroundColor: Colors.grey[100],
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            _appbar()
+            _appbar(),
+            _username(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _username() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: common_gap),
+      child: Text('username', style: TextStyle(fontWeight: FontWeight.bold),),
     );
   }
 
@@ -21,8 +31,6 @@ class ProfileScreen extends StatelessWidget {
               SizedBox(width: 44,),
               Expanded(child: Text("songyunsang", textAlign: TextAlign.center, style: TextStyle(fontSize: 17),)),
               IconButton(icon: Icon(Icons.menu), onPressed: (){})
-
-
             ],
           );
   }
