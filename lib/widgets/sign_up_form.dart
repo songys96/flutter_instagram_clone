1a87d12a9ab0838e5d6b7e125f0b55dbcab87878
yunsang <songys96@naver.com>
2021-01-28 14:57:30 +0900

    41. TextFormField, TextEditingController, validator, InputDecoration, OutlineInputBorder

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -9,6 +9,13 @@ class SignUpForm extends StatefulWidget {
 class _SignUpFormState extends State<SignUpForm> {
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +25,28 @@ class _SignUpFormState extends State<SignUpForm> {
         children: [
           SizedBox(height: common_l_gap,),
           Image.asset("assets/images/insta_text_logo.png"),
-          TextFormField(),
+          Padding(
+            padding: const EdgeInsets.all(common_gap),
+            child: TextFormField(
+              controller: _emailController,
+              validator: (text){
+                if (text.isNotEmpty && text.contains("@"))
+                  return null;
+                else
+                  return "Check your Email address";
+              },
+              decoration: InputDecoration(
+                hintText: "Email",
+                filled: true,
+                fillColor: Colors.grey[100],
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(common_s_gap)
+                )
+              )
+            ),
+
+          ),
           TextFormField(),
           TextFormField(),
         ],
