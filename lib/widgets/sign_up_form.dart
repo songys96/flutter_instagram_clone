60e2202f12aceaa71951228e30d5e301be015fe2
yunsang <songys96@naver.com>
2021-01-28 14:13:10 +0900

    40. signup listview textFromField, GlobalKey

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/constants/common_size.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Form(
+      key: _formKey,
+      child: ListView(
+        children: [
+          SizedBox(height: common_l_gap,),
+          Image.asset("assets/images/insta_text_logo.png"),
+          TextFormField(),
+          TextFormField(),
+          TextFormField(),
+        ],
+      ),
     );
   }
 }
