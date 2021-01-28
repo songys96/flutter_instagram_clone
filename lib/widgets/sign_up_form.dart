a9116e345117d492d2756f3ec894867f0fbfa97e
yunsang <songys96@naver.com>
2021-01-28 17:26:13 +0900

    47. divider with stack (---or---)

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -89,7 +89,27 @@ class _SignUpFormState extends State<SignUpForm> {
                   ),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
-                  )),
+                  )
+              ),
+              SizedBox(height: common_gap,),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    left: 0, right: 0, height: 1,
+                    child: Container(
+                      height: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Container(
+                    height: 3,
+                    width: 40,
+                    color: Colors.grey[50],
+                  ),
+                  Text("OR", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),)
+                ],
+              )
             ],
           ),
         ),
